0ac83f7a0c8aefbc77522fa9e24b4cfb08d83d7d
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-06-05 17:59:41 +0300

    feat: refactor main.py to include additional command handlers and storage in dispatcher

--- bot/main.py
+++ bot/main.py
@@ -7,26 +7,38 @@ from aiogram.client.default import DefaultBotProperties
 from config import BOT_TOKEN
 from bot.dispatcher import register_handlers
 from bot.set_bot_commands import set_bot_commands
+from bot.storage import storage
 from core.handlers.commands import (
     cmd_scan_file,
     cmd_scan_url,
-    cmd_start
+    cmd_start,
+    cmd_link_group,
+    cmd_my_groups
 )
+from core.handlers.commands.group_commands import (
+    cmd_id,
+)
+
+from db import mongo
+
+from core.handlers.callbacks import callbacks
 
 timeout = ClientTimeout(total=60)
 
 async def main():
     bot = Bot(token=BOT_TOKEN, timeout=timeout, default=DefaultBotProperties(parse_mode="HTML"))
     
-    # await set_bot_instance(bot)
-
-    dp = Dispatcher()
+    dp = Dispatcher(storage=storage)
 
     register_handlers(dp)
     await set_bot_commands(bot)
     
     print("Bot is up and running...")
     await dp.start_polling(bot)
+
+    # db logics
+    await mongo.init()
+
 
 if __name__ == '__main__':
     asyncio.run(main())
