35985ed01b602d0ae500abbea2a1a103a7e8e1ac
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-06-05 17:57:13 +0300

    feat: add MONGO_DB_CONNECTION_STRING retrieval and validation

--- config/secrets.py
+++ config/secrets.py
@@ -6,6 +6,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN").replace("\\x3a", ":")
 VIRUS_TOTAL_API_KEY = os.getenv("VIRUS_TOTAL_API_KEY")
 SAFE_BROWSING_API_KEY = os.getenv("SAFE_BROWSING_API_KEY")
+MONGO_DB_CONNECTION_STRING = os.getenv("MONGO_DB_CONNECTION_STRING")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN not found in environment variables")
@@ -16,3 +17,5 @@ if not VIRUS_TOTAL_API_KEY:
 if not SAFE_BROWSING_API_KEY:
     raise ValueError("SAFE_BROWSING_API_KEY not found in environment variables")
 
+if not MONGO_DB_CONNECTION_STRING:
+    raise ValueError("MONGO_DB_CONNECTION_STRING not found in environment variables")
