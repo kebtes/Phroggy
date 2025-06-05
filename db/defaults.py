c64c495668d4b8f91982c28314a7d195c82aceb5
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-06-05 18:04:20 +0300

    default group settings configuration

---
+++ db/defaults.py
@@ -0,0 +1,14 @@
+DEFAULT_GROUP_SETTINGS = {
+    "whitelist": [],
+    "blacklist": [],
+    "moderators": [],
+    "spam_sensitivity": "moderate",
+    "virus_sensitivity": "moderate",
+    "blacklist_keywords": [],
+    "auto_delete": True,
+    "notify_admins": True,
+    "notify_users": True,
+    "skip_files": [],
+    "skip_urls": [],
+    "pause_scan": False
+}
