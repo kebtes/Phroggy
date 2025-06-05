559ec7134caa9135979302144cb10cb189e16e9d
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-06-05 18:01:34 +0300

    feat: implement group linking and management commands

---
+++ core/handlers/commands/cmd_link_group.py
@@ -0,0 +1,88 @@
+from uuid import uuid4
+
+from aiogram import types
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+
+from bot.states import AddToGroupStates
+from core.handlers.commands import router
+from db import users, groups, tokens
+
+current_prompts = []
+
+@router.message(Command("link_group"))
+async def waiting_for_id(message: types.Message, state: FSMContext):
+    uuid = str(uuid4())
+
+    response_msg = (
+        "<b>To add this bot to a group, follow the following steps.</b>\n\n"
+        "<b> 1.</b> Add the bot to the group where you want it to operate.\n\n"
+        "<b> 2.</b> Copy and paste the following command with the token inside that group chat. This will activate the bot’s ability to recognize the group.\n\n"
+        f"<b>Command:</b> <code><b>/id {uuid}</b></code>\n\n"
+        "<b>TOKEN EXPIRES IN 10 MINS</b>"
+    )
+
+    user_id = message.chat.id
+    await tokens.store_token(uuid, user_id)
+    current_prompts.append(await message.reply(response_msg))
+    await state.set_state(AddToGroupStates.waiting_for_id)
+
+@router.message(AddToGroupStates.waiting_for_id)
+async def waiting_for_id(message: types.Message, group_id: int, user_id: int):
+    # group details
+    group_info = await message.bot.get_chat(group_id)
+    group_name = group_info.title
+    group_type = "Public" if group_info.username else "Private"
+    active_usernames = group_info.active_usernames
+    invite_link = group_info.invite_link
+
+    # user details
+    # user_id = message.from_user.id
+    user_name = message.from_user.username
+
+    # ! TODO : check if the group is a super group
+    # ! some funcitonalities might not work for it 
+
+    # create the user
+    user = await users.create_user(
+        username=user_name,
+        user_id=user_id,
+        groups=[group_id]
+    )
+
+    if "error" in user:
+        if error == "VALIDATION_ERROR":
+            await message.bot.send_message(user_id, "Error happend while trying to connect the bot to the group. Please try again later...")
+            return
+        
+    # create the group
+    group = await groups.create_group(
+        group_id=group_id,
+        group_name=group_name,
+        admin=user_id
+    )
+
+    if "error" in group:
+        error = group["error"]
+
+        if error == "VALIDATION_ERROR":
+            await message.bot.send_message(user_id, "Error happend while trying to connect the bot to the group. Please try again later...")
+            return
+            
+    global current_prompts
+    for msg in current_prompts:
+        await message.bot.delete_message(chat_id=user_id, message_id=msg.message_id)
+
+    response_msg = (
+        "<b><u>Bot Successfully Added to Group</u></b>\n\n"
+        f"<b>Group Name:</b> {group_name}\n"
+        f"<b>Group Type:</b> {group_type}\n"
+        f"<b>Active Username:</b> {active_usernames}\n"
+        f"<b>Invitation Link:</b> {invite_link}\n\n"
+        f"The username <code><b>{user_name}</b></code> has been designated as the group admin, with full access to all bot commands. "
+        "Moderators can also be added and will have access to use the bot as well.\n\n"
+        "Use the /help command to see all available commands and see how to use this bot within your group."
+    )
+
+    await message.bot.send_message(user_id, response_msg)
+    current_prompts = []
---
+++ core/handlers/commands/cmd_my_groups.py
@@ -0,0 +1,56 @@
+"""
+
+This command should handle
+
+- list all linked groups by the user
+- for each listed group
+    - group removal/unlink      : group should be removed from db
+    - ability to change admin   : should prompt the user to be sure
+    - ability to get log history for each : (this can wait till log history is fully impl) 
+
+"""
+
+from aiogram import types
+from aiogram.filters import Command
+from aiogram.fsm.context import FSMContext
+
+from bot.states import MyGroupStates
+from core.handlers.commands import router
+from core.keyboards import create
+from db import users, groups
+
+@router.message(Command("my_groups"))
+@router.callback_query(MyGroupStates.group_listing)
+async def list_groups(message: types.Message, state: FSMContext):
+    user_id = message.chat.id
+    user_groups = await users.get_groups(user_id) 
+    
+    if user_groups:
+        buttons = []
+        for group_id in user_groups:
+            info = await groups.group_info(group_id)
+            group_name = info.get("group_name") if info else None
+            group_id = info.get("group_id") if info else None
+            if group_name:
+                buttons.append([group_name, f"info:group:{group_id}"])
+
+        reply_markup = create(
+            buttons=buttons,
+            per_row=2
+        )
+
+        response_msg = (
+            "Below is a list of all the groups you've added me to. "
+            "Tap on any group to either unlink it or update its admin settings."
+        )
+
+        await message.reply(response_msg, reply_markup=reply_markup)
+        await state.set_state(MyGroupStates.waiting_for_group_choice)
+    
+    else:
+        response_msg = (
+            "Looks like you haven't added me to any groups yet!\n\n"
+            "Use /link_group to get started."
+        )
+
+        await message.reply(response_msg)
