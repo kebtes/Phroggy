ca762c12c497b7b28326ca5a4c6bb75388a8e90a
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-06-12 16:31:43 +0300

    feat: add message listener router to handler registration

--- bot/dispatcher.py
+++ bot/dispatcher.py
@@ -1,9 +1,11 @@
 from aiogram import Dispatcher
 from core.handlers import commands 
 from core.handlers.callbacks import callbacks
+from core.handlers import message_listner
 
 def register_handlers(dp: Dispatcher):
     dp.include_routers(
         commands.router,
-        callbacks.router
+        callbacks.router,
+        message_listner.router
     )
