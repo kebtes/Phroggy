c15feafb1b385c8be550d5a2ea8055fd6818593b
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-06-15 20:56:42 +0300

    feat: update start command message

--- core/handlers/commands/cmd_start.py
+++ core/handlers/commands/cmd_start.py
@@ -3,11 +3,13 @@ from aiogram.filters import CommandStart
 
 from core.handlers.commands import router
 
+
 @router.message(CommandStart())
 async def start_command(message: types.Message):
     await message.answer(
-        "👋 Hello! I’m your group protection assistant.\n"
-        "I’ll keep this chat safe from spam, scams, phishing links, and other threats.\n"
-        "Add me to a group and make me admin to begin.\n"
-        "Use /help to view available commands."
+        "<b><u>Phroggy</u></b>\n\n"
+        "I’m your AI-powered group guard — built to block shady links, sketchy files, "
+        "and also spammy messages using a highly accurate pretrained model.\n\n"
+        "Want to see what I can do?\n"
+        "Check the /help command and let’s keep your group clean and secure."
     )
