10d31ed4410866158e4e67ef730591e508215390
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-06-15 15:39:07 +0300

    feat: add notify command for group preference management

---
+++ core/handlers/commands/group_commands/cmd_notify.py
@@ -0,0 +1,44 @@
+from aiogram import types
+from aiogram.filters import Command, CommandObject
+
+from core.handlers.commands import router
+from db import groups
+
+
+@router.message(Command("notify"))
+async def id_group(message: types.Message, command: CommandObject):
+    user_id = message.from_user.id
+    group_id = message.chat.id
+
+    if not (
+        await groups.is_admin(group_id, user_id)
+        or await groups.is_moderator(group_id, user_id)
+    ):
+        await message.reply("Commands are only available to admins/moderators.")
+        return
+
+    # check if args is empty
+    if not command.args:
+        await message.reply("Invalid Format.")
+        return
+
+    # check if the arg format is correct
+    args = command.args.strip().split()
+    if len(args) != 1:
+        await message.reply("Invalid Format.")
+        return
+
+    try:
+        auto_delete = args[0].lower()
+
+        if auto_delete not in ("t", "true", "f", "false"):
+            await message.reply("Options are either t/f")
+            return
+
+        auto_delete = auto_delete == "t" or auto_delete == "true"
+        await groups.update_preference(group_id=group_id, notify=auto_delete)
+
+        await message.reply("Preference updated!")
+
+    except Exception as e:
+        print(e)
