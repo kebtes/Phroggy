bddb0b7c0dbd893978b8ddb6e4a3a79581f7f6f3
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2025-06-15 15:35:04 +0300

    refactor: reorganize imports for better readability and structure

--- bot/main.py
+++ bot/main.py
@@ -1,37 +1,46 @@
 import asyncio
-from aiohttp import ClientTimeout
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from aiohttp import ClientTimeout
 
-from config import BOT_TOKEN
-from bot.dispatcher import register_handlers
-from bot.set_bot_commands import set_bot_commands
-from bot.storage import storage
-from db import mongo
+# Callback Handlers
+import core.handlers.callbacks.callbacks  # noqa: F401
+import core.handlers.commands.cmd_link_group  # noqa: F401
+import core.handlers.commands.cmd_my_groups  # noqa: F401
 
 # Command Handlers (side-effect imports for registration)
 import core.handlers.commands.cmd_scan_file  # noqa: F401
 import core.handlers.commands.cmd_scan_url  # noqa: F401
 import core.handlers.commands.cmd_start  # noqa: F401
-import core.handlers.commands.cmd_link_group  # noqa: F401
-import core.handlers.commands.cmd_my_groups  # noqa: F401
+import core.handlers.commands.group_commands.cmd_add_moderators  # noqa: F401
+import core.handlers.commands.group_commands.cmd_auto_delete  # noqa: F401
+import core.handlers.commands.group_commands.cmd_blacklist  # noqa: F401
+import core.handlers.commands.group_commands.cmd_blacklist_kw  # noqa: F401
+import core.handlers.commands.group_commands.cmd_history  # noqa: F401
 import core.handlers.commands.group_commands.cmd_id  # noqa: F401
-import core.handlers.message_listner  # noqa: F401
-
-# Callback Handlers
-import core.handlers.callbacks.callbacks  # noqa: F401
+import core.handlers.commands.group_commands.cmd_sensitivity  # noqa: F401
+import core.handlers.listeners.message_listner  # noqa: F401
+from bot.dispatcher import register_handlers
+from bot.set_bot_commands import set_bot_commands
+from bot.storage import storage
+from config import BOT_TOKEN
+from db import mongo
 
 timeout = ClientTimeout(total=60)
 
 async def main():
-    bot = Bot(token=BOT_TOKEN, timeout=timeout, default=DefaultBotProperties(parse_mode="HTML"))
-    
+    bot = Bot(
+        token=BOT_TOKEN,
+        timeout=timeout,
+        default=DefaultBotProperties(parse_mode="HTML")
+    )
+
     dp = Dispatcher(storage=storage)
 
     register_handlers(dp)
     await set_bot_commands(bot)
-    
+
     print("Bot is up and running...")
     await dp.start_polling(bot)
 
